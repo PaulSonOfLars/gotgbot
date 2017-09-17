38d2500847605c086eb2b54c7c0c372b197bb526
Paul Larsen <pn1315@ic.ac.uk>
2017-09-17 15:40:54 +0100

    add basic methods for games and payments.

--- library/Ext/games.go
+++ library/Ext/games.go
@@ -1,1 +1,101 @@
 package Ext
+
+import (
+	"bot/library/Types"
+	"log"
+	"encoding/json"
+	"strconv"
+)
+
+// TODO: reply version
+// TODO: no notif version
+func (b Bot) SendGame(chat_id int, game_short_name string) Types.Message {
+	m := make(map[string]string)
+	m["chat_id"] = strconv.Itoa(chat_id)
+	m["game_short_name"] = game_short_name
+
+	r := Get(b, "sendGame", m)
+	if !r.Ok {
+		log.Fatal("You done goofed, API Res for sendGame was not OK")
+	}
+
+	var mess Types.Message
+	json.Unmarshal(r.Result, &mess)
+
+	return mess
+}
+
+
+// TODO Check return - message or bool?
+// TODO: Force version
+// TODO: possible error
+func (b Bot) SetGameScore(user_id int, score int, chat_id int, message_id int) bool {
+	m := make(map[string]string)
+	m["user_id"] = strconv.Itoa(user_id)
+	m["score"] = strconv.Itoa(score)
+	m["chat_id"] = strconv.Itoa(chat_id)
+	m["message_id"] = strconv.Itoa(message_id)
+
+	r := Get(b, "setGameScore", m)
+	if !r.Ok {
+		log.Fatal("You done goofed, API Res for sendGame was not OK")
+	}
+
+	var bb bool
+	json.Unmarshal(r.Result, &bb)
+
+	return bb
+}
+
+// TODO Check return - message or bool?
+// TODO: Force version
+// TODO: Possible error
+func (b Bot) SetGameScoreInline(user_id int, score int, inline_message_id string) bool {
+	m := make(map[string]string)
+	m["user_id"] = strconv.Itoa(user_id)
+	m["score"] = strconv.Itoa(score)
+	m["inline_message_id"] = inline_message_id
+
+	r := Get(b, "setGameScore", m)
+	if !r.Ok {
+		log.Fatal("You done goofed, API Res for sendGame was not OK")
+	}
+
+	var bb bool
+	json.Unmarshal(r.Result, &bb)
+
+	return bb
+}
+
+func (b Bot) GetGameHighScores(user_id int, chat_id int, message_id int) []Types.GameHighScore {
+	m := make(map[string]string)
+	m["user_id"] = strconv.Itoa(user_id)
+	m["chat_id"] = strconv.Itoa(chat_id)
+	m["message_id"] = strconv.Itoa(message_id)
+
+	r := Get(b, "getGameHighScores", m)
+	if !r.Ok {
+		log.Fatal("You done goofed, API Res for sendGame was not OK")
+	}
+
+	var ghs []Types.GameHighScore
+	json.Unmarshal(r.Result, &ghs)
+
+	return ghs
+}
+
+func (b Bot) GetGameHighScoresInline(user_id int, inline_message_id string) []Types.GameHighScore {
+	m := make(map[string]string)
+	m["user_id"] = strconv.Itoa(user_id)
+	m["inline_message_id"] = inline_message_id
+
+	r := Get(b, "getGameHighScores", m)
+	if !r.Ok {
+		log.Fatal("You done goofed, API Res for sendGame was not OK")
+	}
+
+	var ghs []Types.GameHighScore
+	json.Unmarshal(r.Result, &ghs)
+
+	return ghs
+}
--- library/Ext/payments.go
+++ library/Ext/payments.go
@@ -1,1 +1,74 @@
 package Ext
+
+import (
+	"bot/library/Types"
+	"strconv"
+	"log"
+	"encoding/json"
+)
+// TODO: all the optionals here. Best option is probably to use a builder.
+func (b Bot) SendInvoice(chat_id int, title string, description string, payload string,
+						provider_token string, start_parameter string, currency string,
+						prices []Types.LabeledPrice) Types.Message {
+	prices_str, err := json.Marshal(prices)
+	if err != nil {
+		log.Println("Err in send_invoice")
+		log.Fatal(err)
+	}
+	m := make(map[string]string)
+	m["chat_id"] = strconv.Itoa(chat_id)
+	m["title"] = title
+	m["description"] = description
+	m["payload"] = payload
+	m["provider_token"] = provider_token
+	m["start_parameter"] = start_parameter
+	m["currency"] = currency
+	m["prices"] = string(prices_str)
+
+	r := Get(b, "sendInvoice", m)
+	if !r.Ok {
+		log.Println("You done goofed")
+		log.Println(r)
+	}
+
+	var mess Types.Message
+	json.Unmarshal(r.Result, &mess)
+
+	return mess
+}
+
+// TODO: shipping options
+// TODO: err_msg
+func (b Bot) AnswerShippingQuery(shipping_query_id string, ok bool) bool {
+	m := make(map[string]string)
+	m["shipping_query_id"] = shipping_query_id
+	m["ok"] = strconv.FormatBool(ok)
+
+	r := Get(b, "answerShippingQuery", m)
+	if !r.Ok {
+		log.Println("You done goofed")
+		log.Println(r)
+	}
+
+	var bb bool
+	json.Unmarshal(r.Result, &bb)
+
+	return bb
+}
+
+func (b Bot) AnswerPreCheckoutQuery(pre_checkout_query_id string, ok bool) bool {
+	m := make(map[string]string)
+	m["pre_checkout_query_id"] = pre_checkout_query_id
+	m["ok"] = strconv.FormatBool(ok)
+
+	r := Get(b, "answerPreCheckoutQuery", m)
+	if !r.Ok {
+		log.Println("You done goofed")
+		log.Println(r)
+	}
+
+	var bb bool
+	json.Unmarshal(r.Result, &bb)
+
+	return bb
+}
