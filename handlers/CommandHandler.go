63b2aad1e354ab7fe1d772495718dadea6402f41
Paul Larsen <pn1315@ic.ac.uk>
2018-05-10 00:20:08 +0100

    Add an ArgsCommand type to allow for simple passing of args to functions

--- handlers/CommandHandler.go
+++ handlers/CommandHandler.go
@@ -6,9 +6,18 @@ import (
 	"github.com/PaulSonOfLars/gotgbot/ext"
 )
 
-type Command struct {
+type baseCommand struct {
 	command  string
+}
+
+type Command struct {
+	baseCommand
 	response func(b ext.Bot, u gotgbot.Update)
+}
+
+type ArgsCommand struct {
+	baseCommand
+	response func(b ext.Bot, u gotgbot.Update, args []string)
 }
 
 func NewCommand(command string, response func(b ext.Bot, u gotgbot.Update)) Command {
@@ -18,11 +27,22 @@ func NewCommand(command string, response func(b ext.Bot, u gotgbot.Update)) Comm
 	return h
 }
 
+func NewArgsCommand(command string, response func(b ext.Bot, u gotgbot.Update, args []string)) ArgsCommand {
+	h := ArgsCommand{}
+	h.command = command
+	h.response = response
+	return h
+}
+
 func (h Command) HandleUpdate(update gotgbot.Update, d gotgbot.Dispatcher) {
 	h.response(d.Bot, update)
 }
 
-func (h Command) CheckUpdate(update gotgbot.Update) (bool, error) {
+func (h ArgsCommand) HandleUpdate(update gotgbot.Update, d gotgbot.Dispatcher){
+	h.response(d.Bot, update, strings.Fields(update.EffectiveMessage.Text)[1:])
+}
+
+func (h baseCommand) CheckUpdate(update gotgbot.Update) (bool, error) {
 	return update.Message != nil && update.Message.Text != "" &&
 		strings.Split(strings.Fields(update.Message.Text)[0], "@")[0] == "/"+h.command, nil
 }
