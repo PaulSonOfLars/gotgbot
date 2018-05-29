818adb340838d1c18b2c8c33f9b588e39c6d4c2c
Paul Larsen <pn1315@ic.ac.uk>
2018-05-29 22:46:40 +0100

    add arguments to all payments funcs

--- ext/payment_sendables.go
+++ ext/payment_sendables.go
@@ -1,1 +1,171 @@
 package ext
+
+import (
+	"github.com/PaulSonOfLars/gotgbot/types"
+	"encoding/json"
+	"github.com/pkg/errors"
+	"net/url"
+	"strconv"
+)
+
+type sendableInvoice struct {
+	bot                       Bot
+	ChatId                    int
+	Title                     string
+	Description               string
+	Payload                   string
+	ProviderToken             string
+	StartParameter            string
+	Currency                  string
+	Prices                    []types.LabeledPrice
+	ProviderData              string
+	PhotoUrl                  string
+	PhotoSize                 int
+	PhotoWidth                int
+	PhotoHeight               int
+	NeedName                  bool
+	NeedPhoneNumber           bool
+	NeedEmail                 bool
+	NeedShippingAddress       bool
+	SendPhoneNumberToProvider bool
+	SendEmailToProvider       bool
+	IsFlexible                bool
+	DisableNotification       bool
+	ReplyToMessageId          int
+	ReplyMarkup               types.ReplyKeyboardMarkup
+}
+
+func (b Bot) NewSendableInvoice(chatId int, title string, description string, payload string, providerToken string, startParameter string, currency string, prices []types.LabeledPrice) *sendableInvoice {
+	return &sendableInvoice{
+		bot:            b,
+		ChatId:         chatId,
+		Title:          title,
+		Description:    description,
+		Payload:        payload,
+		ProviderToken:  providerToken,
+		StartParameter: startParameter,
+		Currency:       currency,
+		Prices:         prices,
+	}
+}
+
+func (i *sendableInvoice) Send() (*Message, error) {
+	pricesStr, err := json.Marshal(i.Prices)
+	if err != nil {
+		return nil, errors.Wrapf(err, "could not marshal invoice prices")
+	}
+	replyMarkup, err := json.Marshal(i.ReplyMarkup)
+	if err != nil {
+		return nil, errors.Wrapf(err, "could not marshal invoice reply markup")
+	}
+
+	v := url.Values{}
+	v.Add("chat_id", strconv.Itoa(i.ChatId))
+	v.Add("title", i.Title)
+	v.Add("description", i.Description)
+	v.Add("payload", i.Payload)
+	v.Add("provider_token", i.ProviderToken)
+	v.Add("start_parameter", i.StartParameter)
+	v.Add("currency", i.Currency)
+	v.Add("prices", string(pricesStr))
+	v.Add("provider_data", i.ProviderData)
+	v.Add("photo_url", i.PhotoUrl)
+	v.Add("photo_size", strconv.Itoa(i.PhotoSize))
+	v.Add("photo_width", strconv.Itoa(i.PhotoWidth))
+	v.Add("photo_height", strconv.Itoa(i.PhotoHeight))
+	v.Add("need_name", strconv.FormatBool(i.NeedName))
+	v.Add("need_phone_number", strconv.FormatBool(i.NeedPhoneNumber))
+	v.Add("need_email", strconv.FormatBool(i.NeedEmail))
+	v.Add("need_shipping_address", strconv.FormatBool(i.NeedShippingAddress))
+	v.Add("send_phone_number_to_provider", strconv.FormatBool(i.SendPhoneNumberToProvider))
+	v.Add("send_email_to_provider", strconv.FormatBool(i.SendEmailToProvider))
+	v.Add("is_flexible", strconv.FormatBool(i.IsFlexible))
+	v.Add("disable_notification", strconv.FormatBool(i.DisableNotification))
+	v.Add("reply_to_message_id", strconv.Itoa(i.ReplyToMessageId))
+	v.Add("reply_markup", string(replyMarkup))
+
+	r, err := Get(i.bot, "sendInvoice", v)
+	if err != nil {
+		return nil, errors.New(r.Description)
+	}
+	if !r.Ok {
+		return nil, errors.New(r.Description)
+	}
+
+	return i.bot.ParseMessage(r.Result), nil
+}
+
+type sendableAnswerShippingQuery struct {
+	bot             Bot
+	ShippingQueryId string
+	Ok              bool
+	ShippingOptions []types.ShippingOption
+	ErrorMessage    string
+}
+
+func (b Bot) NewSendableAnswerShippingQuery(shippingQueryId string, ok bool) *sendableAnswerShippingQuery {
+	return &sendableAnswerShippingQuery{ShippingQueryId: shippingQueryId, Ok: ok}
+}
+
+func (asq *sendableAnswerShippingQuery) Send() (bool, error) {
+	shippingOptions, err := json.Marshal(asq.ShippingOptions)
+	if err != nil {
+		return false, errors.Wrapf(err, "could not marshal shipping query shipping options")
+	}
+
+	v := url.Values{}
+	v.Add("shipping_query_id", asq.ShippingQueryId)
+	v.Add("ok", strconv.FormatBool(asq.Ok))
+	v.Add("shipping_options", string(shippingOptions))
+	v.Add("error_message", asq.ErrorMessage)
+
+	r, err := Get(asq.bot, "answerShippingQuery", v)
+	if err != nil {
+		return false, errors.Wrapf(err, "unable to answerShippingQuery")
+	}
+	if !r.Ok {
+		return false, errors.New("invalid answerShippingQuery")
+	}
+
+	var bb bool
+	json.Unmarshal(r.Result, &bb)
+	return bb, nil
+}
+
+
+type sendableAnswerPreCheckoutQuery struct {
+	bot             Bot
+	ShippingQueryId string
+	Ok              bool
+	ShippingOptions []types.ShippingOption
+	ErrorMessage    string
+}
+
+func (b Bot) NewSendableAnswerPreCheckoutQuery(shippingQueryId string, ok bool) *sendableAnswerPreCheckoutQuery {
+	return &sendableAnswerPreCheckoutQuery{ShippingQueryId: shippingQueryId, Ok: ok}
+}
+
+func (apcq *sendableAnswerPreCheckoutQuery) Send() (bool, error) {
+	shippingOptions, err := json.Marshal(apcq.ShippingOptions)
+	if err != nil {
+		return false, errors.Wrapf(err, "could not marshal pre checkout query shipping options")
+	}
+
+	v := url.Values{}
+	v.Add("shipping_query_id", apcq.ShippingQueryId)
+	v.Add("ok", strconv.FormatBool(apcq.Ok))
+	v.Add("shipping_options", string(shippingOptions))
+	v.Add("error_message", apcq.ErrorMessage)
+
+	r, err := Get(apcq.bot, "answerPreCheckoutQuery", v)
+	if err != nil {
+		return false, errors.Wrapf(err, "unable to answerPreCheckoutQuery")
+	}
+	if !r.Ok {
+		return false, errors.New("invalid answerPreCheckoutQuery")
+	}
+
+	var bb bool
+	json.Unmarshal(r.Result, &bb)
+	return bb, nil
+}
--- ext/payments.go
+++ ext/payments.go
@@ -2,76 +2,19 @@ package ext
 
 import (
 	"github.com/PaulSonOfLars/gotgbot/types"
-	"strconv"
-	"encoding/json"
-	"net/url"
-	"github.com/pkg/errors"
 )
 
 // TODO: all the optionals here. Best option is probably to use a builder.
 func (b Bot) SendInvoice(chatId int, title string, description string, payload string,
 	providerToken string, startParameter string, currency string,
 	prices []types.LabeledPrice) (*Message, error) {
-	pricesStr, err := json.Marshal(prices)
-	if err != nil {
-		return nil, errors.Wrapf(err, "could not unmarshal invoice prices")
-	}
-	v := url.Values{}
-	v.Add("chat_id", strconv.Itoa(chatId))
-	v.Add("title", title)
-	v.Add("description", description)
-	v.Add("payload", payload)
-	v.Add("provider_token", providerToken)
-	v.Add("start_parameter", startParameter)
-	v.Add("currency", currency)
-	v.Add("prices", string(pricesStr))
-
-	r, err := Get(b, "sendInvoice", v)
-	if err != nil {
-		return nil, errors.New(r.Description)
-	}
-	if !r.Ok {
-		return nil, errors.New(r.Description)
-	}
-
-	return b.ParseMessage(r.Result), nil
-
+	return b.NewSendableInvoice(chatId, title, description, payload, providerToken, startParameter, currency, prices).Send()
 }
 
-// TODO: shipping options
-// TODO: err_msg
 func (b Bot) AnswerShippingQuery(shippingQueryId string, ok bool) (bool, error) {
-	v := url.Values{}
-	v.Add("shipping_query_id", shippingQueryId)
-	v.Add("ok", strconv.FormatBool(ok))
-
-	r, err := Get(b, "answerShippingQuery", v)
-	if err != nil {
-		return false, errors.Wrapf(err, "unable to answerShippingQuery")
-	}
-	if !r.Ok {
-		return false, errors.New("invalid answerShippingQuery")
-	}
-
-	var bb bool
-	json.Unmarshal(r.Result, &bb)
-	return bb, nil
+	return b.NewSendableAnswerShippingQuery(shippingQueryId, ok).Send()
 }
 
 func (b Bot) AnswerPreCheckoutQuery(preCheckoutQueryId string, ok bool) (bool, error) {
-	v := url.Values{}
-	v.Add("pre_checkout_query_id", preCheckoutQueryId)
-	v.Add("ok", strconv.FormatBool(ok))
-
-	r, err := Get(b, "answerPreCheckoutQuery", v)
-	if err != nil {
-		return false, errors.Wrapf(err, "unable to answerPreCheckoutQuery")
-	}
-	if !r.Ok {
-		return false, errors.New("invalid answerPreCheckoutQuery")
-	}
-
-	var bb bool
-	json.Unmarshal(r.Result, &bb)
-	return bb, nil
+	return b.NewSendableAnswerPreCheckoutQuery(preCheckoutQueryId, ok).Send()
 }
