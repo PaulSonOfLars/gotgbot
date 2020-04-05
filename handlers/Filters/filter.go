7ddbadad7ae508eda7e87c3f08d2a00ce35b782e
Paul Larsen <pnvlarsen@gmail.com>
2020-04-05 21:23:10 +0100

    Add a pin filter

--- handlers/Filters/filter.go
+++ handlers/Filters/filter.go
@@ -86,6 +86,10 @@ func Group(message *ext.Message) bool {
 	return message.Chat.Type == "group" || message.Chat.Type == "supergroup"
 }
 
+func Pin(message *ext.Message) bool {
+	return message.PinnedMessage != nil
+}
+
 func Dice(message *ext.Message) bool {
 	return message.Dice != nil
 }
