7c96710b1c8891d36961dcf8a50ba9b0b92a41b3
Paul Larsen <pnvlarsen@gmail.com>
2021-04-07 21:46:12 +0100

    Replies in private should still reply

--- custom_helpers.go
+++ custom_helpers.go
@@ -5,7 +5,7 @@ func (m Message) Reply(b *Bot, text string, opts *SendMessageOpts) (*Message, er
 		opts = &SendMessageOpts{}
 	}
 
-	if opts.ReplyToMessageId == 0 && m.Chat.Type != "private" {
+	if opts.ReplyToMessageId == 0 {
 		opts.ReplyToMessageId = m.MessageId
 	}
 
