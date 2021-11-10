6b90317d1dbcc8aa4fdfd19e136fa5eaa84a3822
Paul Larsen <pnvlarsen@gmail.com>
2021-11-10 23:21:37 +0000

    Update error string

--- scripts/ci/generate.sh
+++ scripts/ci/generate.sh
@@ -7,6 +7,7 @@ go generate
 # Check if a diff is found. If yes, fail.
 diff="$(git diff)"
 if [[ -n "$diff" ]]; then
-  echo "A diff was found when generating the docs" >&2
+  echo "A diff was found when generating the docs. Please commit the changes." >&2
   exit 1
 fi
+echo "No diff found, all is well!" >&2
