ce018035ec630ab209d0dd3967ca9dffce86860b
Paul Larsen <pnvlarsen@gmail.com>
2021-12-10 14:34:58 +0000

    Add docs

--- ext/sender.go
+++ ext/sender.go
@@ -4,6 +4,8 @@ import (
 	"github.com/PaulSonOfLars/gotgbot/v2"
 )
 
+// Sender is a merge of the User and SenderChat fields of a message, to provide easier interaction with
+// message senders from the telegram API.
 type Sender struct {
 	User *gotgbot.User
 	Chat *gotgbot.Chat
@@ -12,6 +14,7 @@ type Sender struct {
 	ChatId             int64
 }
 
+// GetSender populates the relevant fields of a Sender struct given a message.
 func GetSender(msg *gotgbot.Message) *Sender {
 	return &Sender{
 		User:               msg.From,
@@ -21,6 +24,7 @@ func GetSender(msg *gotgbot.Message) *Sender {
 	}
 }
 
+// Id determines the real sender ID.
 func (s Sender) Id() int64 {
 	if s.Chat != nil {
 		return s.Chat.Id
@@ -31,6 +35,7 @@ func (s Sender) Id() int64 {
 	return 0
 }
 
+// Username determines the real sender username.
 func (s Sender) Username() string {
 	if s.Chat != nil {
 		return s.Chat.Username
@@ -41,22 +46,46 @@ func (s Sender) Username() string {
 	return ""
 }
 
+// Name determines the real name of the sender.
+// This is:
+// - chat.Title for a chat.
+// - user.Firstname + user.Lastname for a user (the full name).
+func (s Sender) Name() string {
+	if s.Chat != nil {
+		return s.Chat.Title
+	}
+	if s.User != nil {
+		if s.User.LastName == "" {
+			return s.User.FirstName
+		}
+		return s.User.FirstName + " " + s.User.LastName
+	}
+	return ""
+}
+
+// IsUser returns true if the Sender is a user (including bot).
 func (s Sender) IsUser() bool {
 	return s.Chat == nil && s.User != nil
 }
 
+// IsBot returns true if the Sender is a bot.
+// Returns false if the user is a bot setup by telegram for backwards compatibility with
+// the sender_chat fields.
 func (s Sender) IsBot() bool {
 	return s.Chat == nil && s.User != nil && s.User.IsBot
 }
 
+// IsAnonymousAdmin returns true if the Sender is an anonymous admin.
 func (s Sender) IsAnonymousAdmin() bool {
 	return s.Chat != nil && s.Chat.Id == s.ChatId
 }
 
+// IsAnonymousChannel returns true if the Sender is an anonymous channel.
 func (s Sender) IsAnonymousChannel() bool {
 	return s.Chat != nil && s.Chat.Id != s.ChatId && !s.IsAutomaticForward
 }
 
+// IsLinkedChannel returns true if the Sender is a linked channel.
 func (s Sender) IsLinkedChannel() bool {
 	return s.Chat != nil && s.Chat.Id != s.ChatId && s.IsAutomaticForward
 }
