c695d9a18686047d739c9778ce0f3ed1bb14b2f6
Paul Larsen <pnvlarsen@gmail.com>
2021-12-11 00:15:52 +0000

    More helper methods to make the Sender type more user-like

--- sender.go
+++ sender.go
@@ -19,7 +19,9 @@ func (m Message) GetSender() *Sender {
 	}
 }
 
-// Id determines the real sender ID.
+// Id determines the sender ID.
+// When a message is being sent by a chat/channel, telegram usually populates the User field with dummy values.
+// For this reason, we prefer to return the Chat.Id if it is available, rather than a dummy User.Id.
 func (s Sender) Id() int64 {
 	if s.Chat != nil {
 		return s.Chat.Id
@@ -30,7 +32,7 @@ func (s Sender) Id() int64 {
 	return 0
 }
 
-// Username determines the real sender username.
+// Username determines the sender username.
 func (s Sender) Username() string {
 	if s.Chat != nil {
 		return s.Chat.Username
@@ -41,10 +43,10 @@ func (s Sender) Username() string {
 	return ""
 }
 
-// Name determines the real name of the sender.
+// Name determines the name of the sender.
 // This is:
-// - chat.Title for a chat.
-// - user.Firstname + user.Lastname for a user (the full name).
+// - Chat.Title for a Chat.
+// - User.FirstName + User.LastName for a User (the full name).
 func (s Sender) Name() string {
 	if s.Chat != nil {
 		return s.Chat.Title
@@ -58,7 +60,35 @@ func (s Sender) Name() string {
 	return ""
 }
 
-// IsUser returns true if the Sender is a user (including bot).
+// FirstName determines the firstname of the sender.
+// This is:
+// - Chat.Title for a Chat.
+// - User.FirstName for a User.
+func (s Sender) FirstName() string {
+	if s.Chat != nil {
+		return s.Chat.Title
+	}
+	if s.User != nil {
+		return s.User.FirstName
+	}
+	return ""
+}
+
+// LastName determines the firstname of the sender.
+// This is:
+// - empty for a Chat.
+// - User.LastName for a User.
+func (s Sender) LastName() string {
+	if s.Chat != nil {
+		return "" // empty; we define the "title" as being a firstname, so there is no lastname.
+	}
+	if s.User != nil {
+		return s.User.LastName
+	}
+	return ""
+}
+
+// IsUser returns true if the Sender is a User (including bot).
 func (s Sender) IsUser() bool {
 	return s.Chat == nil && s.User != nil
 }
