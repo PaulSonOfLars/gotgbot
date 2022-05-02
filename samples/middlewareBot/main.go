c2275322c09af9da1464b6936268339bee04654f
Paul Larsen <pnvlarsen@gmail.com>
2022-05-02 15:13:04 +0100

    Add comments to the middleware example

--- samples/middlewareBot/main.go
+++ samples/middlewareBot/main.go
@@ -17,27 +17,32 @@ import (
 
 // Define middleware BotClient
 type sendWithoutReplyBotClient struct {
-	// Inline existing client to defer to it when middleware complete
-	// This is necessary to support API future methods.
+	// Inline existing client to call, allowing us to chain middlewares.
+	// Inlining also avoids us having to redefine helper methods part of the interface.
 	gotgbot.BotClient
 }
 
 // Define wrapper around existing PostWithContext method.
 // Note: this is the only method that needs redefining.
 func (lb *sendWithoutReplyBotClient) PostWithContext(ctx context.Context, method string, params map[string]string, data map[string]gotgbot.NamedReader, opts *gotgbot.RequestOpts) (json.RawMessage, error) {
+	// For all sendable methods, we want to allow sending if the message has been deleted.
+	// So, we edit the params to allow for that.
+	// We also log this, for the sake of the example. :)
 	if strings.HasPrefix(method, "send") || method == "copyMessage" {
 		fmt.Println("Applying middleware to", method)
 		params["allow_sending_without_reply"] = "true"
 	}
 
+	// Call the child n
 	val, err := lb.BotClient.PostWithContext(ctx, method, params, data, opts)
 	if err != nil {
+		// Middlewares can also be used to increase error visibility, in case they aren't logged elsewhere.
 		fmt.Println("warning, got an error:", err)
 	}
 	return val, err
 }
 
-// Define simple method to instantiate middleware.
+// SendWithoutReply is a simple method that we use to wrap the existing middleware with our new one.
 func SendWithoutReply(b gotgbot.BotClient) gotgbot.BotClient {
 	return &sendWithoutReplyBotClient{b}
 }
