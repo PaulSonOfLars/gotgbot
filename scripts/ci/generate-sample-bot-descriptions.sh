ee206b63d466f8024415a9305e4e51165a2ddd97
Paul Larsen <pnvlarsen@gmail.com>
2023-01-01 23:23:47 +0000

    Add generated README

--- scripts/ci/generate-sample-bot-descriptions.sh
+++ scripts/ci/generate-sample-bot-descriptions.sh
@@ -6,9 +6,10 @@ README_FILE="${SAMPLES_DIR}/README.md"
 
 # Single >, to recreate file
 
-intro="# Sample bot descriptions
+intro="# Sample bots
 
-This is a short introduction of all the sample bots present in this directory.
+This is a short description of all the sample bots present in this directory. These are intended to be a source of
+inspiration and learning for anyone looking to use this library.
 "
 
 echo "$intro" >"${README_FILE}"
