b6176f15c3637a55ec0666f83af5c29a8eccfa70
Paul Larsen <pnvlarsen@gmail.com>
2023-04-23 11:12:28 +0100

    Latest changes

--- gen_types.go
+++ gen_types.go
@@ -3034,7 +3034,7 @@ func (v InlineQueryResultVoice) inlineQueryResult() {}
 type InlineQueryResultsButton struct {
 	// Label text on the button
 	Text string `json:"text"`
-	// Optional. Description of the Web App that will be launched when the user presses the button. The Web App will be able to switch back to the inline mode using the method web_app_switch_inline_query inside the Web App.
+	// Optional. Description of the Web App that will be launched when the user presses the button. The Web App will be able to switch back to the inline mode using the method switchInlineQuery inside the Web App.
 	WebApp *WebAppInfo `json:"web_app,omitempty"`
 	// Optional. Deep-linking parameter for the /start message sent to the bot when a user presses the button. 1-64 characters, only A-Z, a-z, 0-9, _ and - are allowed. Example: An inline bot that sends YouTube videos can ask the user to connect the bot to their YouTube account to adapt search results accordingly. To do this, it displays a 'Connect your YouTube account' button above the results, or even before showing any. The user presses the button, switches to a private chat with the bot and, in doing so, passes a start parameter that instructs the bot to return an OAuth link. Once done, the bot can offer a switch_inline button so that the user can easily return to the chat where they wanted to use the bot's inline capabilities.
 	StartParameter string `json:"start_parameter,omitempty"`
