6d5070b1d18394a552f15f752940c0068e93c2d0
Paul Larsen <pnvlarsen@gmail.com>
2023-08-21 00:23:50 +0200

    Create example metrics bot to show off using the processor for metrics

---
+++ samples/metricsBot/metricsMiddleware.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+
+	"github.com/PaulSonOfLars/gotgbot/v2"
+)
+
+var (
+	totalRequests = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: "gotgbot",
+			Name:      "requests_total",
+			Help:      "Number of requests made to the bot API.",
+		},
+		[]string{
+			"api_method",
+		},
+	)
+	totalHTTPErrors = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: "gotgbot",
+			Name:      "http_request_errors_total",
+			Help:      "Number of HTTP errors obtained.",
+		},
+		[]string{
+			"api_method",
+		},
+	)
+	totalAPIErrors = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: "gotgbot",
+			Name:      "api_request_errors_total",
+			Help:      "Number of bot API errors obtained.",
+		},
+		[]string{
+			"api_method",
+			"api_status_code",
+			"description",
+		},
+	)
+	requestDuration = promauto.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: "gotgbot",
+			Name:      "api_request_time_seconds",
+			Help:      "Duration of requests made to the bot API.",
+		},
+		[]string{
+			"api_method",
+		},
+	)
+)
+
+// Define middleware BotClient
+type metricsBotClient struct {
+	// Inline existing client to call, allowing us to chain middlewares.
+	// Inlining also avoids us having to redefine helper methods part of the interface.
+	gotgbot.BotClient
+}
+
+// Define wrapper around existing RequestWithContext method.
+// Note: this is the only method that needs redefining.
+func (b *metricsBotClient) RequestWithContext(ctx context.Context, method string, params map[string]string, data map[string]gotgbot.NamedReader, opts *gotgbot.RequestOpts) (json.RawMessage, error) {
+	totalRequests.WithLabelValues(method).Inc()
+	timer := prometheus.NewTimer(requestDuration.With(prometheus.Labels{
+		"http_method": http.MethodPost,
+		"api_method":  method,
+	}))
+
+	val, err := b.BotClient.RequestWithContext(ctx, method, params, data, opts)
+	timer.ObserveDuration()
+	if err != nil {
+		tgErr := &gotgbot.TelegramError{}
+		if errors.As(err, &tgErr) {
+			totalAPIErrors.WithLabelValues(method, strconv.Itoa(tgErr.Code), tgErr.Description).Inc()
+		} else {
+			totalHTTPErrors.WithLabelValues(method).Inc()
+		}
+	}
+	return val, err
+}
+
+// metrics enables the metrics middleware.
+// To ensure metrics are accurate, we recommend calling this middleware first.
+func metrics(b gotgbot.BotClient) gotgbot.BotClient {
+	return &metricsBotClient{b}
+}
---
+++ samples/metricsBot/metricsProcessor.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+
+	"github.com/PaulSonOfLars/gotgbot/v2"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext"
+)
+
+var (
+	totalUpdates = promauto.NewCounter(
+		prometheus.CounterOpts{
+			Namespace: "gotgbot",
+			Name:      "updates_total",
+			Help:      "Number of incoming updates.",
+		},
+	)
+	updateProcessingDuration = promauto.NewHistogram(
+		prometheus.HistogramOpts{
+			Namespace: "gotgbot",
+			Name:      "update_processing_time_seconds",
+			Help:      "Time to process each update.",
+		},
+	)
+	bufferedUpdates = promauto.NewGauge(
+		prometheus.GaugeOpts{
+			Namespace: "gotgbot",
+			Name:      "buffered_updates",
+			Help:      "Number of updates currently buffered in the dispatcher limiter channel.",
+		},
+	)
+	bufferedUpdatesLimit = promauto.NewGauge(
+		prometheus.GaugeOpts{
+			Namespace: "gotgbot",
+			Name:      "buffered_updates_limit",
+			Help:      "Maximum number of buffered updates in the limiter channel.",
+		},
+	)
+)
+
+var _ ext.Processor = metricsProcessor{}
+
+type metricsProcessor struct {
+	processor ext.Processor
+}
+
+func (m metricsProcessor) ProcessUpdate(d *ext.Dispatcher, b *gotgbot.Bot, u *gotgbot.Update, data map[string]interface{}) error {
+	totalUpdates.Inc()
+	timer := prometheus.NewTimer(updateProcessingDuration)
+	defer timer.ObserveDuration()
+
+	return m.processor.ProcessUpdate(d, b, u, data)
+}
+
+func monitorDispatcherBuffer(d *ext.Dispatcher) {
+	// Limiter cant be changed during execution, so only needs to be set once.
+	bufferedUpdatesLimit.Set(float64(d.MaxUsage()))
+
+	for {
+		bufferedUpdates.Set(float64(d.CurrentUsage()))
+		time.Sleep(time.Second)
+	}
+}
