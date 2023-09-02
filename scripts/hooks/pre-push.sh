3712ef2b6ea4189569501380899f364033ce61c6
Paul Larsen <pnvlarsen@gmail.com>
2023-09-02 13:16:03 +0100

    Add output information on prepush hooks

--- scripts/hooks/pre-push.sh
+++ scripts/hooks/pre-push.sh
@@ -5,19 +5,23 @@
 # it has execute permissions.
 set -euo pipefail
 
+echo "Linting project..."
 # Run golangci-lint to lint against all the code in the lib.
 golangci-lint run
 
-# Run golangci-lint against
+
+# Run golangci-lint against samples
+echo "Linting samples..."
 for d in ./samples/*; do
   if [[ ! -d "$d" ]]; then
     continue
   fi
 
-  pushd "$d"
+  pushd "$d" >/dev/null
   golangci-lint run --config=../../.golangci.yaml --path-prefix="$d"
-  popd
+  popd >/dev/null
 done
 
+echo "Checking generated docs are up to date..."
 # Ensure all generated code has up to date.
 ./scripts/ci/ensure-generated.sh >/dev/null
