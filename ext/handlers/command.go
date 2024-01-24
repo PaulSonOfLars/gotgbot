d21da03dd1683e95b9be71fce9bbb4c90680c643
Paul Larsen <pnvlarsen@gmail.com>
2024-01-24 22:13:16 +0000

    Add triggers helper, to ease customising command triggers (#140)

--- ext/handlers/command.go
+++ ext/handlers/command.go
@@ -8,14 +8,21 @@ import (
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
 )
 
+// Command is the go-to handler for setting up Commands in your bot. By default, it will use telegram-native commands
+// that start with a forward-slash (/), but it can be customised to react to any message starting with a character.
+//
+// For example, a command handler on "help" with the triggers []rune("/!,") would trigger for "/help", "!help", or ",help".
 type Command struct {
 	Triggers     []rune
 	AllowEdited  bool
 	AllowChannel bool
-	Command      string // should be lowercase for case-insensitivity
+	Command      string // set to a lowercase value for case-insensitivity
 	Response     Response
 }
 
+// NewCommand creates a new case-insensitive command.
+// By default, commands do not work on edited messages, or channel posts. These can be enabled by setting the
+// AllowEdited and AllowChannel fields respectively.
 func NewCommand(c string, r Response) Command {
 	return Command{
 		Triggers:     []rune{'/'},
@@ -35,6 +42,12 @@ func (c Command) SetAllowEdited(allow bool) Command {
 // SetAllowChannel Enables channel messages for this handler.
 func (c Command) SetAllowChannel(allow bool) Command {
 	c.AllowChannel = allow
+	return c
+}
+
+// SetTriggers sets the list of triggers to be used with this command.
+func (c Command) SetTriggers(triggers []rune) Command {
+	c.Triggers = triggers
 	return c
 }
 
