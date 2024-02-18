89b22dc66086f9e4ec04c1ca9fab0f6da3b42bf8
Paul Larsen <pnvlarsen@gmail.com>
2024-02-18 16:53:22 +0000

    Add additional sender helpers

--- ext/handlers/filters/types.go
+++ ext/handlers/filters/types.go
@@ -13,5 +13,5 @@ type (
 	PollAnswer         func(pa *gotgbot.PollAnswer) bool
 	PreCheckoutQuery   func(pcq *gotgbot.PreCheckoutQuery) bool
 	ShippingQuery      func(sq *gotgbot.ShippingQuery) bool
-	Reaction           func(mra *gotgbot.MessageReactionUpdated) bool
+	Reaction           func(mru *gotgbot.MessageReactionUpdated) bool
 )
