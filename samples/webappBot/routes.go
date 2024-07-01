78bf4a4a53901051e455dbcd9cf665445d75b255
Paul Larsen <pnvlarsen@gmail.com>
2024-07-02 00:31:34 +0200

    Improve webapp sample comments and usage

--- samples/webappBot/routes.go
+++ samples/webappBot/routes.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"net/url"
 	"text/template"
 
+	"github.com/PaulSonOfLars/gotgbot/v2"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
 )
 
@@ -24,17 +28,38 @@ func index(webappURL string) func(writer http.ResponseWriter, request *http.Requ
 }
 
 func validate(token string) func(writer http.ResponseWriter, request *http.Request) {
-	return func(writer http.ResponseWriter, request *http.Request) {
-		ok, err := ext.ValidateWebAppQuery(request.URL.Query(), token)
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Our index.html sends the WebApp.initData field over the X-Auth header.
+		// We parse this string as a URL query.
+		authQuery, err := url.ParseQuery(r.Header.Get("X-Auth"))
 		if err != nil {
-			writer.WriteHeader(http.StatusBadRequest)
-			writer.Write([]byte("validation failed; error: " + err.Error()))
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("validation failed; failed to parse auth query: " + err.Error()))
+		}
+
+		// We validate that the query has been hashed correctly, ensuring data can be trusted.
+		ok, err := ext.ValidateWebAppQuery(authQuery, token)
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("validation failed; error: " + err.Error()))
 			return
 		}
-		if ok {
-			writer.Write([]byte("validation success; user is authenticated."))
-		} else {
-			writer.Write([]byte("validation failed; data cannot be trusted."))
+		if !ok {
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("validation failed; data cannot be trusted."))
+			return
 		}
+
+		// Once we've confirmed the data can be trusted, we unmarshal any data we may need to use.
+		var u gotgbot.User
+		err = json.Unmarshal([]byte(authQuery.Get("user")), &u)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("validation failed; failed to unmarshal user: " + err.Error()))
+			return
+		}
+
+		// And then we can choose to either return it, or work with it.
+		w.Write([]byte(fmt.Sprintf("validation success; user '%s' is authenticated (id: %d).", u.FirstName, u.Id)))
 	}
 }
