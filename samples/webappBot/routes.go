56282dbfcc5db78c7f6482463eb0fdabb6a95237
Paul Larsen <pnvlarsen@gmail.com>
2025-11-06 23:13:08 +0100

    fix lint

--- samples/webappBot/routes.go
+++ samples/webappBot/routes.go
@@ -22,7 +22,7 @@ func index(webappURL string) func(writer http.ResponseWriter, request *http.Requ
 		})
 		if err != nil {
 			writer.WriteHeader(http.StatusInternalServerError)
-			writer.Write([]byte(err.Error()))
+			fmt.Fprint(writer, err.Error())
 		}
 	}
 }
@@ -34,19 +34,19 @@ func validate(token string) func(writer http.ResponseWriter, request *http.Reque
 		authQuery, err := url.ParseQuery(r.Header.Get("X-Auth"))
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("validation failed; failed to parse auth query: " + err.Error()))
+			fmt.Fprint(w, "validation failed; failed to parse auth query: "+err.Error())
 		}
 
 		// We validate that the query has been hashed correctly, ensuring data can be trusted.
 		ok, err := ext.ValidateWebAppQuery(authQuery, token)
 		if err != nil {
 			w.WriteHeader(http.StatusUnauthorized)
-			w.Write([]byte("validation failed; error: " + err.Error()))
+			fmt.Fprint(w, "validation failed; error: "+err.Error())
 			return
 		}
 		if !ok {
 			w.WriteHeader(http.StatusUnauthorized)
-			w.Write([]byte("validation failed; data cannot be trusted."))
+			fmt.Fprint(w, "validation failed; data cannot be trusted.")
 			return
 		}
 
@@ -55,11 +55,11 @@ func validate(token string) func(writer http.ResponseWriter, request *http.Reque
 		err = json.Unmarshal([]byte(authQuery.Get("user")), &u)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("validation failed; failed to unmarshal user: " + err.Error()))
+			fmt.Fprint(w, "validation failed; failed to unmarshal user: "+err.Error())
 			return
 		}
 
 		// And then we can choose to either return it, or work with it.
-		w.Write([]byte(fmt.Sprintf("validation success; user '%s' is authenticated (id: %d).", u.FirstName, u.Id)))
+		fmt.Fprintf(w, "validation success; user '%s' is authenticated (id: %d).", u.FirstName, u.Id)
 	}
 }
