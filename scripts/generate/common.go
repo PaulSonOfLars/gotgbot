5165fc55ce9b8f7c0896db5de373b0354b1c5238
Paul Larsen <pnvlarsen@gmail.com>
2026-06-25 18:45:23 +0200

    fix merge bit

--- scripts/generate/common.go
+++ scripts/generate/common.go
@@ -400,14 +400,6 @@ func childFieldTypesMatch(d APIDescription, parentType string, fields []Field) b
 	return true
 }
 
-func (f *Field) ConstantName(parentName string, shortName string) string {
-	return ConstantName(parentName, f.Name, shortName)
-}
-
-func ConstantName(parentName string, fieldName string, shortName string) string {
-	return snakeToTitle(parentName) + snakeToTitle(fieldName) + snakeToTitle(shortName)
-}
-
 func unique(ss ...string) []string {
 	seen := make(map[string]struct{})
 	unique := make([]string, 0, len(ss))
